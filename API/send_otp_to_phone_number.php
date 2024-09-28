9533b33341e8154a4e7e51b4102d098f82c8b01a
aradhya-jain-codes <rishi.jain.mee@gmail.com>
2024-09-28 21:01:34 +0530

    required phone no. for OTP

--- API/send_otp_to_phone_number.php
+++ API/send_otp_to_phone_number.php
@@ -13,11 +13,21 @@ function generateOTP() {
     return $otp;
 }
 
+// Check if the phone number is provided
+if (!isset($_REQUEST['phone_number']) || empty($_REQUEST['phone_number'])) {
+    // Set the content type to application/json
+    header('Content-Type: application/json');
+    
+    // Return an error message as a JSON object
+    echo json_encode(['error' => 'Phone number is required.']);
+    exit;
+}
+
 // Generate the OTP
 $otp = generateOTP();
 
 // Set the content type to application/json
 header('Content-Type: application/json');
 
-// Return the OTP as a JSON object
-echo json_encode(['otp' => $otp]);
+// Return the OTP and phone number as a JSON object
+echo json_encode(['otp' => $otp, 'phone_number' => $_REQUEST['phone_number']]);
