15b1dcadb55edaf86c8d9727f3d64323669e7193
aradhya-jain-codes <rishi.jain.mee@gmail.com>
2024-09-30 02:08:15 +0530

    Re-classification of files into guard & student

---
+++ API/student/upload_profile_photo.php
@@ -0,0 +1,41 @@
+<?php
+// File: upload.php
+
+// Define the target directory
+// include '../../debug_config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json'); 
+
+
+if (!isset($_REQUEST['scholar_no']) || empty($_REQUEST['scholar_no'])) {
+    header('Content-Type: application/json');
+    echo json_encode(['status' => 'error', 'message' => 'Scholar number is required.']);
+    exit;
+}
+$scholar_no = $_REQUEST['scholar_no'];
+$target_dir = "../../photos/";
+
+if (!is_dir($target_dir)) {
+    mkdir($target_dir, 0755, true);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
+        $file_name = basename($_FILES["image"]["name"]);
+        $target_file = $target_dir . $scholar_no .".png";
+        
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+            echo json_encode(["success" => true, "message" => "File uploaded successfully", "file" => $target_file]);
+        } else {
+            echo json_encode(["success" => false, "message" => "Failed to upload file"]);
+        }
+    } else {
+        echo json_encode(["success" => false, "message" => "No file was uploaded"]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "Invalid request method"]);
+}
+
