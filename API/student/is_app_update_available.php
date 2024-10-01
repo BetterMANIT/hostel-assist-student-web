c3c1757787858d820c337320fd39f88c29f2ce1e
Anuj Shrivastava <179094956+anuj-softech@users.noreply.github.com>
2024-10-02 00:44:56 +0530

    Update is_app_update_available.php

--- API/student/is_app_update_available.php
+++ API/student/is_app_update_available.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 // Latest version code and APK download link (can be fetched from a database)
 $latest_version_code = 5; // Example: the latest version code of the app
-$apk_download_link = "https://example.com/latest_app.apk"; // Replace with actual APK link
+$apk_download_link = "https://www.apkmirror.com/apk/sud-inc/dr-driving-2/dr-driving-2-1-70-release/dr-driving-1-70-android-apk-download/download/?key=042e21660ea70cbf193e0179b987f5eb3450c54a&forcebaseapk=true"; // Replace with actual APK link
 
 // Check if version_code is provided in the POST request
 if (isset($_POST['version_code'])) {
