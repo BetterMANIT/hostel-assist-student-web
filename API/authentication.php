0603d7f87f50165de9958b2865bb5ce91005a364
aradhya-jain-codes <rishi.jain.mee@gmail.com>
2025-02-15 11:08:56 +0530

    auth

--- API/authentication.php
+++ API/authentication.php
@@ -14,8 +14,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST' && $_SERVER['REQUEST_METHOD'] !== 'GET
 $headers = getallheaders();
 $scholar_no = $_POST['scholar_no'] ?? $_GET['scholar_no'] ?? null;
 $phone_no = $_POST['phone_no'] ?? $_GET['phone_no'] ?? null;
-$device_id = $headers['device_id'] ?? null;
-$token = $headers['token'] ?? null;
+$device_id = $headers['device_id'] ??  $_POST['device_id'] ?? null;
+$token = $headers['token'] ??  $_POST['token'] ?? null;;
 
 // if (!$device_id || !$token || (!$scholar_no && !$phone_no)) {
 //     respond('error', 'Missing required parameters');
